db40da2c854e2bab68ad8025e43a841e9af94d88
Lucas Meyer <lucas.meyer@microsoft.com>
2025-02-04 15:17:08 -0600

    Added configuration changes functionality

--- src/eg4_solar_client/client.py
+++ src/eg4_solar_client/client.py
@@ -1,8 +1,5 @@
 import requests
 import logging
-from dotenv import load_dotenv
-from pprint import pprint
-import os
 
 logging.basicConfig(
     level=logging.INFO,  # Change to DEBUG for more granular logs
@@ -12,7 +9,9 @@ logger = logging.getLogger("eg4_solar_client")
 
 
 class EG4Client:
-    def __init__(self, account: str, password: str, device_serial_number: str):
+    def __init__(
+        self, account: str, password: str, gridboss_sn: str, main_inverter_sn: str
+    ):
         self.session = requests.Session()
 
         # Credentials
@@ -21,7 +20,8 @@ class EG4Client:
             "password": password,
         }
 
-        self.device_serial_number = device_serial_number
+        self.gridboss_sn = gridboss_sn
+        self.main_inverter_sn = main_inverter_sn
 
         response = self.session.post(
             "https://monitor.eg4electronics.com/WManage/web/login", data=payload
@@ -33,7 +33,7 @@ class EG4Client:
 
     def get_inverter_energy_info(self):
         payload = {
-            "serialNum": self.device_serial_number,
+            "serialNum": self.gridboss_sn,
         }
         response = self.session.post(
             "https://monitor.eg4electronics.com/WManage/api/midbox/getMidboxRuntime",
@@ -46,17 +46,170 @@ class EG4Client:
         else:
             logger.error("Failed to get inverter energy info:", response.text)
 
+    def get_month_summary(self, year: str, month: str):
+        payload = {
+            "serialNum": self.gridboss_sn,
+            "year": year,
+            "month": month,
+        }
+        response = self.session.post(
+            "https://monitor.eg4electronics.com/WManage/api/inverterChart/monthColumnParallel",
+            data=payload,
+        )
 
-def main():
-    eg4_client = EG4Client(
-        account=os.getenv("EG4_ACCOUNT"),
-        password=os.getenv("EG4_PASSWORD"),
-        device_serial_number=os.getenv("EG4_DEVICE_SN"),
-    )
+        if response.status_code == 200:
+            logger.info("Successfully got EG4 monthly summary")
+            return response.json()
+        else:
+            logger.error("Failed to get monthly summary:", response.text)
 
-    data = eg4_client.get_inverter_energy_info()
-    pprint(data)
+    def set_working_mode(self, working_mode: str, enable: bool):
+        endpoint = "https://monitor.eg4electronics.com/WManage/web/maintain/remoteSet/functionControl"
 
-if __name__ == "__main__":
-    load_dotenv()
-    main()
+        # Make sure the working mode is valid
+        if working_mode not in ["PV_CHARGE_PRIORITY", "AC_CHARGE_PRIORITY"]:
+            raise ValueError(
+                "working_mode must be 'PV_CHARGE_PRIORITY' or 'AC_CHARGE_PRIORITY'"
+            )
+
+        # convert working mode to the internal EG4 name
+        if working_mode == "PV_CHARGE_PRIORITY":
+            working_mode_name = "HOLD_FORCED_CHG_EN"
+
+        elif working_mode == "AC_CHARGE_PRIORITY":
+            working_mode_name = "FUNC_AC_CHARGE"
+
+        # Make sure the enable value is a boolean
+        if not isinstance(enable, bool):
+            raise ValueError("enable must be a boolean")
+
+        # convert from boolean to string
+        enable_str = "true" if enable else "false"
+
+        payload = {
+            "inverterSn": self.main_inverter_sn,
+            "functionParam": working_mode_name,
+            "enable": enable_str,
+            "clientType": "WEB",
+            "remoteSetType": "NORMAL",
+        }
+
+        response = self.session.post(endpoint, data=payload)
+
+        if response.status_code == 200:
+            logger.info("Successfully set working mode")
+            return True
+
+    def set_interval(self, working_mode: str, start_time: str, end_time: str):
+        # Make sure the working mode is valid
+        if working_mode not in ["PV_CHARGE_PRIORITY", "AC_CHARGE_PRIORITY"]:
+            raise ValueError(
+                "working_mode must be 'PV_CHARGE_PRIORITY' or 'AC_CHARGE_PRIORITY'"
+            )
+
+        # Make sure the start and end time are in the correct format
+        if len(start_time.split(":")) != 2:
+            raise ValueError("start_time must be in the format HH:MM")
+
+        if len(end_time.split(":")) != 2:
+            raise ValueError("end_time must be in the format HH:MM")
+
+        # parse the start and end time into start_hour, start_min, end_hour, end_min
+        start_hour, start_min = start_time.split(":")
+        end_hour, end_min = end_time.split(":")
+
+        # Make sure the start and end time are valid
+        if int(start_hour) < 0 or int(start_hour) > 23:
+            raise ValueError("start_hour must be between 0 and 23")
+
+        if int(start_min) < 0 or int(start_min) > 59:
+            raise ValueError("start_min must be between 0 and 59")
+
+        if int(end_hour) < 0 or int(end_hour) > 23:
+            raise ValueError("end_hour must be between 0 and 23")
+
+        if int(end_min) < 0 or int(end_min) > 59:
+            raise ValueError("end_min must be between 0 and 59")
+
+        # Now that the data is validated, we need to create two payloads, one to set the start and another one to set the end
+        # convert working mode to the internal EG4 name
+        if working_mode == "PV_CHARGE_PRIORITY":
+            working_mode_name = "HOLD_FORCED_CHARGE"
+        elif working_mode == "AC_CHARGE_PRIORITY":
+            working_mode_name = "HOLD_AC_CHARGE"
+
+        # if the start time is different from the end time, ensure the function is enabled
+        if start_time != end_time:
+            self.set_working_mode(working_mode=working_mode, enable=True)
+
+        endpoint = "https://monitor.eg4electronics.com/WManage/web/maintain/remoteSet/writeTime"
+
+        payload_start = {
+            "inverterSn": self.main_inverter_sn,
+            "timeParam": f"{working_mode_name}_START_TIME",
+            "hour": start_hour,
+            "minute": start_min,
+            "clientType": "WEB",
+            "remoteSetType": "NORMAL",
+        }
+
+        payload_end = {
+            "inverterSn": self.main_inverter_sn,
+            "timeParam": f"{working_mode_name}_END_TIME",
+            "hour": end_hour,
+            "minute": end_min,
+            "clientType": "WEB",
+            "remoteSetType": "NORMAL",
+        }
+
+        response_start = self.session.post(endpoint, data=payload_start)
+        response_end = self.session.post(endpoint, data=payload_end)
+
+        if response_start.status_code == 200 and response_end.status_code == 200:
+            logger.info("Successfully set PV charge priority interval")
+            return True
+
+    def prioritize_battery_pv_charging(
+        self, start_time: str = None, end_time: str = None
+    ):
+        if start_time is not None and end_time is not None:
+            self.set_interval(
+                working_mode="PV_CHARGE_PRIORITY",
+                start_time=start_time,
+                end_time=end_time,
+            )
+        else:
+            self.set_interval(
+                working_mode="AC_CHARGE_PRIORITY", start_time="00:00", end_time="00:00"
+            )
+            self.set_interval(
+                working_mode="PV_CHARGE_PRIORITY", start_time="00:00", end_time="23:59"
+            )
+
+        return True
+
+    def prioritize_battery_ac_charging(
+        self, start_time: str = None, end_time: str = None
+    ):
+        if start_time is not None and end_time is not None:
+            self.set_interval(
+                working_mode="AC_CHARGE_PRIORITY",
+                start_time=start_time,
+                end_time=end_time,
+            )
+        else:
+            self.set_interval(
+                working_mode="PV_CHARGE_PRIORITY", start_time="00:00", end_time="23:59"
+            )
+
+            self.set_interval(
+                working_mode="AC_CHARGE_PRIORITY", start_time="00:00", end_time="23:59"
+            )
+
+        return True
+
+    def set_default_config(self):
+        self.prioritize_battery_ac_charging("16:30", "18:00")
+        self.prioritize_battery_pv_charging("11:00", "18:00")
+
+        return True
--- tests/test_midbox.py
+++ tests/test_midbox.py
@@ -1,36 +1,30 @@
-import os
-import pytest
-from eg4_solar_client import EG4Client  # Adjust the import as needed
-from dotenv import load_dotenv
+from .eg4 import EG4Test
 
-class TestEG4Client:
-
+class TestEG4ClientRead(EG4Test):
     def test_get_inverter_energy_info_integration(self):
-        account = os.getenv("EG4_ACCOUNT")
-        password = os.getenv("EG4_PASSWORD")
-        device_serial_number = os.getenv("EG4_DEVICE_SN")
 
-        # skip the test if the credentials are not present.
-        if account is None or password is None or device_serial_number is None:
-            pytest.skip("EG4_ACCOUNT, EG4_PASSWORD, and EG4_DEVICE_SN must be set in the environment to run this test")
-
-        # Instantiate the client.
-        client = EG4Client(
-            account=account,
-            password=password,
-            device_serial_number=device_serial_number
-        )
+        client = self.get_client()
 
         # Make the actual request to fetch inverter energy info.
         data = client.get_inverter_energy_info()
 
         # Assert that the data was returned successfully.
-        assert data is not None, "Expected non-null data from the inverter energy info endpoint"
+        assert data is not None, (
+            "Expected non-null data from the inverter energy info endpoint"
+        )
 
-@pytest.fixture(scope="session", autouse=True)
-def load_env_vars():
-    load_dotenv()
-    assert os.getenv("EG4_ACCOUNT") is not None, "EG4_ACCOUNT must be set in the environment"
-    assert os.getenv("EG4_PASSWORD") is not None, "EG4_PASSWORD must be set in the environment"
-    assert os.getenv("EG4_DEVICE_SN") is not None, "EG4_DEVICE_SN must be set in the environment"
+    def test_get_month_summary_integration(self):
+
+        client = self.get_client()
+
+        # Make the actual request to fetch month summary.
+        data = client.get_month_summary(year="2025", month="02")
+
+        # Assert that the data was returned successfully.
+        assert data is not None, (
+            "Expected non-null data from the month summary endpoint"
+        )
+
+        # Assert that dayMax == 28
+        assert data["dayMax"] == 28, "Expected dayMax to be 28"
 
